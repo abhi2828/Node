206c54d4bf25dde9dddac618ce5839b997f7a1fd
abhay.mishra <abhay.mishra@qualigytech.com>
2022-12-16 08:22:26 +0530

    nodemon install

---
+++ index.js
@@ -0,0 +1,8 @@
+// const http = require('http') // import http module to handle req and res 
+
+// http.createServer( (req,res) => { 
+//     res.write('Hello, This is abhay') // to get the output on browser
+//     res.end() // to end response else it will run infinitely
+//  }).listen(4500)
+
+console.log('nodemon working')
