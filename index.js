764742c577c31defb2c6a4c15d48b2b5681512e8
abhay.mishra <abhay.mishra@qualigytech.com>
2022-12-18 12:56:35 +0530

    added notes in readme file

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ input[2] == 'add' ? fs.writeFileSync(input[3],input[4]) : input[2] == 'remove' ?
 
 // input:
 
-// 1.  node .\index.js add input.txt 'file added again'
+// 1.  node .\index.js add input.txt 'file added again' 
+// :=> this will create file input.txt with text 'file added again'
 // 2.  node .\index.js remove input.txt
+// :=> this will remove file
 // 3.  node .\index.js input.txt 'file added again'
+// :=> this will show check if file input.txt exist
