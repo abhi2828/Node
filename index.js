028b76b9aae88c2359367327f3aeb8c4c10a92e3
abhay.mishra <abhay.mishra@qualigytech.com>
2022-12-22 13:20:08 +0530

    dynamic page done

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
 const express = require("express")
+const path = require("path")
+
 const app = express()
+const publicPath = path.join(__dirname,'public')
+// console.log(publicPath)
 
+// app.use(express.static(publicPath)) // this is used to create static pages
+
+
+app.set("view engine","ejs")
+// for removing extensions like .html from URL in html or any page we follow bellow methods
 app.get('',(req,res)=>{
-    console.log('req', req.query) // don't understand why we used req.query (query params or query parameters) ??
-    res.send('<h1>welcome, this is home page</h1>')
-})
-
-app.get('/about',(req,res)=>{
-    res.send('Hello, this is about page')
-})
-
-app.get('/contact',(req,res)=>{
-    res.send('Hello, this is contact page')
+    res.sendFile(`${publicPath}/index.html`)
 })
 
 app.get('/help',(req,res)=>{
-    res.send('welcome, this is help page')
+    res.sendFile(`${publicPath}/help.html`)
+})
+
+app.get('/about',(req,res)=>{
+    res.sendFile(`${publicPath}/about.html`)
+})
+
+app.get('/profile',(req,res)=>{
+    const data = {
+        name:"abhay",
+        email:"abhay@test.com",
+        city:"Ulhasnagar"
+    }
+    res.render(`profile`,{data})
+})
+
+// if user don't enter proper URL so, user will redirect to index page
+app.get('*',(req,res)=>{
+    res.sendFile(`${publicPath}/index.html`)  
 })
 
 app.listen(5000)
